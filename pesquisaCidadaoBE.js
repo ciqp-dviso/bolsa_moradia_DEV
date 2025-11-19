29546d7b4b904fa528de67eb6192c1824a359dec
Ruy Braga Filho <ruybragafilho@gmail.com>
2025-11-19 08:11:24 -0300

    Implementação da tela da bolsa moradia

---
+++ pesquisaCidadaoBE.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * Módulo:    pesquisaCidadaoBE.gs
+ * Objetivo:  Pesquisar um cidadão na base do CADUNICO
+ */
+
+
+/**
+ * Função que pesquisa um cidadão, pelo CPF, na base do CADUNICO
+ *  
+ * @param {String} CPF: O CPF do cidadão
+ * 
+ * @return {Objetc} Um objeto com os dados do cidadão
+ */
+function pesquisaCidadaoBE( CPF ) {
+
+} // Fim da função inserirNovoCaso 
+
+
+
+/**
+ * ##### FIM DO MÓDULO pesquisaCidadaoBE.gs #####
+ */
